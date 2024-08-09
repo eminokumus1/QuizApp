295bf51fe998ce536b9562552a3669a4f02f1e9b
eminokumus1 <eminokumus1@gmail.com>
2024-08-09 06:19:14 +0300

    Sign out button added to ProfileFragment

--- app/src/main/java/com/eminokumus/quizapp/profile/ProfileFragment.kt
+++ app/src/main/java/com/eminokumus/quizapp/profile/ProfileFragment.kt
@@ -1,19 +1,26 @@
 package com.eminokumus.quizapp.profile
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.eminokumus.quizapp.MainActivity
 import com.eminokumus.quizapp.Quizzes.QuizzesFragment
 import com.eminokumus.quizapp.databinding.FragmentProfileBinding
+import com.eminokumus.quizapp.login.LoginActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import javax.inject.Inject
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
+
+    private lateinit var auth: FirebaseAuth
 
     @Inject
     lateinit var viewModel: ProfileViewModel
@@ -28,6 +35,8 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+
+        auth = Firebase.auth
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
@@ -49,6 +58,13 @@ class ProfileFragment : Fragment() {
             }
             solvedQuizzesBtn.setOnClickListener {
                 findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToQuizzesFragment(QuizzesFragment.solvedQuizzesScreen))
+            }
+            signOutButton.setOnClickListener {
+                auth.signOut()
+                val intent = Intent(requireContext(), LoginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+
             }
         }
     }
